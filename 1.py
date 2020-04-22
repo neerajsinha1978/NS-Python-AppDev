67de2000f0eef2d2994e0e4c9172f32fb6c30f0d
neerajsinha1978 <neeraj.sa@gmail.com>
2020-04-21 23:40:39 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,35 @@
+# Program 1
+# txt = "Hello World"[::-1]
+# print(txt)
+
+# Program 1 ends
+# Program 2
+
+# def my_function(x):
+#   return x[::-1]
+
+# mytxt = my_function("I wonder how this text looks like backwards")
+
+# print(mytxt)
+
+# Program 2 ends
+# Program 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Program 3 ends
+# Program 4
